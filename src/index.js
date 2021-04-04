6a4e1bc01b7ad2b4418b6f7a08fab3b72cbaa2c2
szhao0102 <13725570410@163.com>
2021-04-04 10:20:49 +0800

    begin react

--- src/index.js
+++ src/index.js
@@ -1,64 +1,50 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-// import { createStore } from 'redux'
+// import PropTypes from 'prop-types'
+import { createStore } from 'redux'
 import ReactDOM from 'react-dom'
-// import { Provider, connect } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 
 class Counter extends Component {
     render() {
-        // const {value, onInc} = this.props
+        const {value, onInc} = this.props
         return (
             <div>
-                1
-                {/* <span>{value}</span>
-                <button onClick={onInc}>Inc</button> */}
+                
+                <span>{value}</span>
+                <button onClick={onInc}>Inc</button>
             </div>
         )
     }
 }
-// const Counter = (props) => {
-//     const {value, onInc} = props
-//     return (
-//         <div>
-//             <span>{value}</span>
-//             <button onClick={onInc}>Inc</button>
-//         </div>
-//     )
-// }
-// Counter.PropTypes = {
-//     value: PropTypes.number.isRequired,
-//     onInc: PropTypes.func.isRequired
-// }
 
-// const Action = {type: "add"}
+const Action = {type: "add"}
 
-// const Reducer = (state = { count: 0}, action) => {
-//     switch(action.type) {
-//         case 'add': return {count: state.count + 1};break;
-//         default: return state
-//     }
-// }
+const Reducer = (state = { count: 0}, action) => {
+    switch(action.type) {
+        case 'add': return {count: state.count + 1};break;
+        default: return state
+    }
+}
 
-// const store = createStore(Reducer)
+const store = createStore(Reducer)
 
-// const mapStateToProps = (state) => {
-//     return {
-//         value: state.count
-//     }
-// }
+const mapStateToProps = (state) => {
+    return {
+        value: state.count
+    }
+}
 
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         onInc: () => dispatch(Action)
-//     }
-// }
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onInc: () => dispatch(Action)
+    }
+}
 
-// const App = connect(mapStateToProps, mapDispatchToProps)(Counter)
+const App = connect(mapStateToProps, mapDispatchToProps)(Counter)
 
 ReactDOM.render(
-    <Counter />,
-    // <Provider store = {store}>
-    //     <App />
-    // </Provider>,
+    <Provider store = {store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 )
